3fa4096e00aa6897ab57bf69b4861632955a283f
brookeangel <brooke.nicole.angel@gmail.com>
2019-12-28 18:32:11 -0800

    Add GraphQL endpoint and schema

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,23 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_graphql import GraphQLView
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Avoid circular dependency
 from app import routes, models
+from schema import schema
+
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True # for having the GraphiQL interface
+    )
+)
